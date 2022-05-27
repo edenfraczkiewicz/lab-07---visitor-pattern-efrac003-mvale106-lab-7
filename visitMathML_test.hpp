e3f427c4292ccde4e55ed3d0075dc5512e9b896b
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-05-27 00:21:43 -0700

    fixed valgrind leaks :D

--- visitMathML_test.hpp
+++ visitMathML_test.hpp
@@ -17,69 +17,85 @@
 
 TEST(VisitMathMLtest, basicOp)
 {   
-    Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
+    	Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
 
-    VisitMathML* mathML = new VisitMathML();
-    std::string expect = "<math>\n  <cn>0</cn>\n</math>\n";    
-    EXPECT_EQ(mathML->PrintMathML(num[0]), expect);
-
-    for (auto i : num) delete i;
-    delete mathML;
+    	VisitMathML* mathML = new VisitMathML();
+    	std::string expect = "<math>\n  <cn>0</cn>\n</math>\n";    
+    	EXPECT_EQ(mathML->PrintMathML(num[0]), expect);
+	
+   	delete mathML;
+   	for(auto i: num) {
+                delete i;
+        }
 }
 
 TEST(VisitMathMLtest, twoComposites)
 {
-    Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
+    	Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
     
-    Base* a = new Sub(num[5], num[0]);
-    Base* test = new Add(num[0], a);
+    	Base* a = new Sub(num[5], num[0]);
+   	Base* test = new Add(num[0], a);
 
-    VisitMathML* mathML = new VisitMathML();
-    std::string expect = "<math>\n  <apply>\n    <plus/>\n    <cn>0</cn>\n    <apply>\n      <minus/>\n      <cn>5</cn>\n      <cn>0</cn>\n    </apply>\n  </apply>\n</math>\n";
-    EXPECT_EQ(mathML->PrintMathML(test), expect);
+    	VisitMathML* mathML = new VisitMathML();
+    	std::string expect = "<math>\n  <apply>\n    <plus/>\n    <cn>0</cn>\n    <apply>\n      <minus/>\n      <cn>5</cn>\n      <cn>0</cn>\n    </apply>\n  </apply>\n</math>\n";
+    	EXPECT_EQ(mathML->PrintMathML(test), expect);
 
-    for (auto i : num) delete i;
-    delete mathML, a, test;
+	delete mathML;
+    	delete a; 
+    	delete test;
+	for(auto i: num) {
+                delete i;
+        }
 }
 
 TEST(VisitMathMLtest, multipleComposites)
 {
-    Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
-
+    	Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
 	Base* a = new Mult(num[2], num[5]);
-
 	Base* b = new Pow(num[5], num[2]);
 
 	Base* test = new Div(a, b);
 
-    VisitMathML* mathML = new VisitMathML();
-    std::string expect = "<math>\n  <apply>\n    <divide/>\n    <apply>\n      <times/>\n      <cn>2</cn>\n      <cn>5</cn>\n    </apply>\n    <apply>\n      <power/>\n      <cn>5</cn>\n      <cn>2</cn>\n    </apply>\n  </apply>\n</math>\n";
-    EXPECT_EQ(mathML->PrintMathML(test), expect);
+    	VisitMathML* mathML = new VisitMathML();
+    	std::string expect = "<math>\n  <apply>\n    <divide/>\n    <apply>\n      <times/>\n      <cn>2</cn>\n      <cn>5</cn>\n    </apply>\n    <apply>\n      <power/>\n      <cn>5</cn>\n      <cn>2</cn>\n    </apply>\n  </apply>\n</math>\n";
+    	EXPECT_EQ(mathML->PrintMathML(test), expect);
 
-    for (auto i : num) delete i;
-    delete mathML, a, b, test;
+    	delete mathML; 
+	delete a; 
+	delete b; 
+	delete test;
+	for(auto i: num) {
+                delete i;
+        }
 }
 
 TEST(VisitMathMLtest, allComposites)
 {
-    Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
-    Base *add, *sub, *mult, *div, *test;
-    Base *neg = new Op(-2);
+    	Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
+    	Base *add, *sub, *mult, *div, *test;
+    	Base *neg = new Op(-2);
 
 
-    add   = new Add(neg,    num[3]);   // 3 + (-2) = 1
-    sub   = new Sub(num[4], add);      // 4 - 1 = 3
-     mult  = new Mult(neg,   sub);      // (-2)  * 3 = -6
-    div   = new Div(mult,   num[4]);   // (-6) / 4 = 1.5
-    test  = new Pow(div,    num[4]);   // 1.5 ^ 4 = 5.0625
+    	add   = new Add(neg,    num[3]);   // 3 + (-2) = 1
+    	sub   = new Sub(num[4], add);      // 4 - 1 = 3
+    	mult  = new Mult(neg,   sub);      // (-2)  * 3 = -6
+    	div   = new Div(mult,   num[4]);   // (-6) / 4 = 1.5
+    	test  = new Pow(div,    num[4]);   // 1.5 ^ 4 = 5.0625
 
-    VisitMathML* mathML = new VisitMathML();
-    std::string expect = "<math>\n  <apply>\n    <power/>\n    <apply>\n      <divide/>\n      <apply>\n        <times/>\n        <cn>-2</cn>\n        <apply>\n          <minus/>\n          <cn>4</cn>\n          <apply>\n            <plus/>\n            <cn>-2</cn>\n            <cn>3</cn>\n          </apply>\n        </apply>\n      </apply>\n      <cn>4</cn>\n    </apply>\n    <cn>4</cn>\n  </apply>\n</math>\n";
-    EXPECT_EQ(mathML->PrintMathML(test), expect);
+    	VisitMathML* mathML = new VisitMathML();
+    	std::string expect = "<math>\n  <apply>\n    <power/>\n    <apply>\n      <divide/>\n      <apply>\n        <times/>\n        <cn>-2</cn>\n        <apply>\n          <minus/>\n          <cn>4</cn>\n          <apply>\n            <plus/>\n            <cn>-2</cn>\n            <cn>3</cn>\n          </apply>\n        </apply>\n      </apply>\n      <cn>4</cn>\n    </apply>\n    <cn>4</cn>\n  </apply>\n</math>\n";
+    	EXPECT_EQ(mathML->PrintMathML(test), expect);
 
-    for (auto i : num) delete i;
-    delete mathML, test;
-    delete add, sub, mult, div;
+	delete mathML;
+	delete test;
+    	delete add;
+	delete sub;
+	delete mult;
+	delete div;
+	delete neg;
+	for(auto i: num) {
+                delete i;
+        }
 }
 
 
