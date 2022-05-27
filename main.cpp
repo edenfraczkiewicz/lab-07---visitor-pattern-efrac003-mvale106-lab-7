e0d97d8eacfccb6aca7b163b64cd325a8be795a9
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-05-27 12:30:06 -0700

    Made examples for Latex and MathML

--- main.cpp
+++ main.cpp
@@ -9,22 +9,95 @@
 #include "rand.hpp"
 #include "sub.hpp"
 #include "visitor_latex.hpp"
+#include "visitMathML.hpp"
 using namespace std;
 
 int main(){
-
 	VisitorLaTeX* latex = new VisitorLaTeX();
+	VisitMathML* mathML = new VisitMathML();	
+	//example 1:
+	Base* op1a = new Op(5);
+	Base* op1b = new Op(1);
+	Base* op1c = new Op(0);
 	
-	Base* val1 = new Op(5);
-	Base* val2 = new Op(2);
-	Base* ex1 = new Add(val1, val2); //7
-	Base* ex2 = new Sub(val1, val2);
-	Base* ex3 = new Mult(val1, val2);
-	Base* ex4 = new Div(val2, val2); //1
-	Base* ex5 = new Pow(val1, val2);
-	Base* ex6 = new Add(ex1, ex4);
-	cout << "Should look like: $(({5.000000}+{2.000000})+(\\frac{{2.000000}}{{2.000000}))$" << endl;
-	cout << "Got: " << latex->PrintLaTeX(ex6) << endl;
-	delete latex;delete val1;delete val2;delete ex1;delete ex2;delete ex3;delete ex4;delete ex5;delete ex6;
+	Base* sub1a = new Sub(op1a, op1c);
+	Base* add1a = new Add(op1b, sub1a);
+	
+	cout << "------------------------------------------------" << endl;
+	cout << "EXAMPLE 1: (1+(5-0))" << endl;
+	cout << "Latex: " << latex->PrintLaTeX(add1a) << endl;
+	cout << "MathML: " << mathML->PrintMathML(add1a) << endl;
+	cout << "------------------------------------------------" << endl;
+
+	//example 2:
+	Base* op2a = new Op(5);
+        Base* op2b = new Op(2);
+
+        Base* pow2a = new Pow(op2a, op2b);
+
+        cout << "EXAMPLE 2: (5^2)" << endl;
+        cout << "Latex: " << latex->PrintLaTeX(pow2a) << endl;
+        cout << "MathML: " << mathML->PrintMathML(pow2a) << endl;
+        cout << "------------------------------------------------" << endl;
+
+	//example 3:
+	Base* op3a = new Op(5);
+        Base* op3b = new Op(2);
+
+        Base* mult3a = new Mult(op3b, op3a);
+
+        cout << "EXAMPLE 3: (2*5)" << endl;
+        cout << "Latex: " << latex->PrintLaTeX(mult3a) << endl;
+        cout << "MathML: " << mathML->PrintMathML(mult3a) << endl;
+        cout << "------------------------------------------------" << endl;
+
+	//example 4:
+	Base* op4a = new Op(2);
+        Base* op4b = new Op(3);
+
+        Base* div4a = new Div(op4a, op4b);
+
+        cout << "EXAMPLE 4: (2/3)" << endl;
+        cout << "Latex: " << latex->PrintLaTeX(div4a) << endl;
+        cout << "MathML: " << mathML->PrintMathML(div4a) << endl;
+        cout << "------------------------------------------------" << endl;
+	
+	//example 5:
+	Base* op5a = new Op(2);
+        Base* op5b = new Op(5);
+
+        Base* div5a = new Div(mult3a, pow2a);
+
+        cout << "EXAMPLE 5: (2*5)/(5^2)" << endl;
+        cout << "Latex: " << latex->PrintLaTeX(div5a) << endl;
+        cout << "MathML: " << mathML->PrintMathML(div5a) << endl;
+        cout << "------------------------------------------------" << endl;
+
+
+	//deleting pointers
+	delete latex;
+	delete mathML;
+	delete op1a;
+	delete op1b;
+	delete op1c;
+	delete sub1a;
+	delete add1a;
+
+	delete op2a;
+	delete op2b;
+	delete pow2a;
+
+	delete op3a;
+	delete op3b;
+	delete mult3a;
+
+	delete op4a;
+	delete op4b;
+	delete div4a;
+
+	delete op5a;
+	delete op5b;
+	delete div5a;
+
 	return 0;
 }
